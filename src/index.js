4b904fad077ac61f3334448923d2dc0f0577a840
SamsonDm <samsondm97@gmail.com>
2023-08-19 21:07:39 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reversedN = 0;
+    n = Math.abs(n);
+    while (n > 0) {
+        reversedN = reversedN * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return reversedN;
 }
